b690b1425ff69dbe8026b93a9a375d0af21e6ba8
VALMIR AMASIO DA SILVA FERNANDES <valmir.asfernandes@senacsp.edu.br>
2018-04-11 08:54:59 -0300

    Declaração de Variáveis

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace PrimeiroGit
     {
         static void Main(string[] args)
         {
-            
+            //declaração das variáveis para guardar os valores
+            //que o usuário entrará
+        double valor1,valor2,resultado;
+       
+
         }
     }
 }
