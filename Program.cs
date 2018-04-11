aac1f81e6d69b78c62f1dfb22f6da00f604c12e3
VALMIR AMASIO DA SILVA FERNANDES <valmir.asfernandes@senacsp.edu.br>
2018-04-11 09:23:30 -0300

    Resultado do calculo

--- Program.cs
+++ Program.cs
@@ -9,12 +9,13 @@ namespace PrimeiroGit
             //declaração das variáveis para guardar os valores
             //que o usuário entrará
         double valor1,valor2,resultado;
-       Console.WriteLine("Digite um valor")
+       Console.WriteLine("Digite um valor");
        valor1 = double.Parse(Console.ReadLine());
 
        Console.WriteLine("Entre com outro valor");
        valor2 = double.Parse(Console.ReadLine());
-
+        resultado = valor1+valor2;
+        
         }
     }
 }
